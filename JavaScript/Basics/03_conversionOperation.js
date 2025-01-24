6dbda9643c42a469f14414c8838502381211fa5a
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2025-01-24 23:53:14 +0530

    add to git

---
+++ JavaScript/Basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = "753asdf"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+let result = null
+
+console.log(typeof result);
+console.log(typeof (result));
+
+let value_in_number = Number(result)
+console.log(typeof value_in_number);
+console.log(value_in_number);
+
+
+
+let result01 = undefined
+
+console.log(typeof result01);
+console.log(typeof (result01));
+
+let value_in_number01 = Number(result01)
+console.log(typeof value_in_number01);
+console.log(value_in_number01);
+
+
+
+let result02 = false
+
+console.log(typeof result02);
+console.log(typeof (result02));
+
+let value_in_number02 = Number(result01)
+console.log(typeof value_in_number02);
+console.log(value_in_number02);
