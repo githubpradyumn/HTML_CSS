52784f6c8259dcf94fe3783a510dbbf83c0f8924
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2025-01-25 20:25:08 +0530

    adding to variable property

--- JavaScript/Basics/01_variables.js
+++ JavaScript/Basics/01_variables.js
@@ -40,3 +40,45 @@ When to Use var, let, or const?
 5. Only use var if you MUST support old browsers.
 
 */
+
+let x = 9;
+x=5
+console.log(x);
+
+
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+// cars = ["Toyota", "Volvo", "Audi"];    // ERROR
+
+
+// You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
+
+
+// You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
+
+const car = {type:"Fiat", model:"500", color:"white"};
+
+car = {type:"Volvo", model:"EX60", color:"red"};    // ERROR
